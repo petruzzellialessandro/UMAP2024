86601be08bac72627a3202799960128f76d5d603
PlaneB22 <47218421+petruzzellialessandro@users.noreply.github.com>
2024-01-24 16:52:31 +0100

    Update bert.py

--- dataloader/bert.py
+++ dataloader/bert.py
@@ -77,7 +77,7 @@ class BertDataloader(AbstractDataloader):
 
     def get_pytorch_dataloaders(self):
         train_loader = self._get_train_loader()
-        val_loader =  self._get_test_loader()#self._get_eval_loader()
+        val_loader =  self._get_eval_loader()
         test_loader = self._get_test_loader()
         return train_loader, val_loader, test_loader
 
